88c5d3a889e3aface2f99faa9ed4f32efc7c2d16
ShinCoder <88935125+ShinCoder@users.noreply.github.com>
2022-08-28 22:54:55 +0700

    Redesign + reposive

---
+++ Number_game.js
@@ -0,0 +1,73 @@
+var userInput;
+var goal;
+
+showUserInput = () => {
+    str = userInput.join("");
+    document.getElementById("input-box").innerHTML = str;
+}
+
+sendInput = (input) => {
+    switch (input) {
+        case "clear":
+            userInput = [];
+            break;
+        case "del":
+            userInput.pop();
+            break;
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            if (userInput.length < 4) {
+                userInput.push(Number(input));
+            }
+            break;
+        default:
+            break;
+    }
+
+    showUserInput();
+    showHint();
+    console.log(goal, userInput);
+}
+
+randomNum = (min,max) => {
+    number = Math.round(Math.random() *(max - min + 1) + min);
+    return number;
+}
+
+startGame = () => {
+    goal = randomNum(1, 9999);
+    userInput = [];
+    
+    showUserInput();
+    showHint();
+    document.getElementById("game-body").style.pointerEvents = "auto";
+}
+
+showHint = () => {
+    let number = Number(userInput.join(""));
+    let message;
+    if (number == 0) {
+        message = "Guess the number!";
+    }
+    else if (number < goal) {
+        message = "Larger";
+    }
+    else if (number > goal) {
+        message = "Smaller";
+    }
+    else if (number == goal) {
+        message = "That's the right number!";
+    }
+    else {
+        message = ""
+    }
+    document.getElementById("hint-box").innerHTML = message;
+}
